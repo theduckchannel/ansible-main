5e15eb66b3c88027749d3ec8ccbcf709484c8bc4
theduckchannel <fredcox@gmail.com>
2021-09-07 16:22:28 -0300

    backup current datetime is: 07092021162228

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ansible-galaxy collection install community.general
-
+nsible-galaxy collection install kewlfft.aur
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/
