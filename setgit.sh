bef681d3420d411fe73fcc0c4f6596cb911d85b1
theduckchannel <fredcox@gmail.com>
2021-09-07 09:17:33 +0000

    backup current datetime is: 07092021091733

--- setgit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git config --global user.email "fredcox@gmail.com"
-git config --global user.name "theduckchannel"
--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git config --global user.email "fredcox@gmail.com"
+git config --global user.name "theduckchannel"
 current_date=$(date +"%d%m%Y%H%M%S")
 git add -A
 git commit -m "backup current datetime is: $current_date"
